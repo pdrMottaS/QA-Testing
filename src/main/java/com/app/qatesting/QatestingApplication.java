e457005f343f47c1374daa786242989a9d25f5d4
Pedro Motta <pedromotta321r@gmail.com>
2023-03-09 19:59:04 -0300

    first commit

---
+++ src/main/java/com/app/qatesting/QatestingApplication.java
@@ -0,0 +1,13 @@
+package com.app.qatesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QatestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QatestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/qatesting/QatestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.qatesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QatestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
